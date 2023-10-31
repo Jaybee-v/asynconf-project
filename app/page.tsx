82841d6cc64c4ebcb1528aca52a03228f2daca68
Vrain Jean-Baptiste <j-b.vrain@outlook.fr>
2023-10-31 02:48:54 +0100

    ended

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ import React from "react"
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center gap-4 md:p-4 relative ">
-            <div className="w-full h-72 object-none absolute top-0 z-10">
+        <main className="flex min-h-screen flex-col items-center gap-4 md:p-4 relative w-screen">
+            <div className="w-screen h-72 object-none absolute top-0 z-10">
                 <Image
                     src="/assets/forest-min.jpg"
                     fill
                     priority
                     alt="Représentation d'une forêt"
-                    className="object-none w-full max-h-72 h-full"
+                    className="object-none w-screen max-h-72 h-full"
                 />
             </div>
             <Header />
--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -7,7 +7,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const labelVariants = cva(
-  "text-sm font-semibold text-primary leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 py-2"
+  "text-sm font-semibold text-background-reverse leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 py-2"
 )
 
 const Label = React.forwardRef<
